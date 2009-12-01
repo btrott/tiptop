32513551eb343c6adc5efcdff84b2902355755fe
Benjamin Trott <ben@sixapart.com>
2009-11-30 22:10:53 -0800

    Added basic machinery for packaging.

---
+++ lib/Dash.pm
@@ -0,0 +1,17 @@
+package Dash;
+use strict;
+use 5.008_001;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 AUTHOR
+
+Benjamin Trott E<lt>ben@sixapart.comE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
